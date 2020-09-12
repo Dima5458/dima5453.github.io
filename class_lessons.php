0da4924047514f0cca04e03e45ddb1416897d246
dima5453 <62203613+dima5453@users.noreply.github.com>
2020-09-12 23:26:28 +0300

    Update class_lessons.php

--- class_lessons.php
+++ class_lessons.php
@@ -1,3 +1,1 @@
-     <link rel="stylesheet" href="" type="text/css" />
-<script type="text/javascript" src="../js/jquery.form.js"></script>
-<h1>запишите урок</h1>
+$sql = "SELECT * FROM `Оценки7б`";
