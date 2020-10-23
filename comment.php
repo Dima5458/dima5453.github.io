a99ee5379b91d9b8128bc4543c19eb6ee4febad9
dima5453 <62203613+dima5453@users.noreply.github.com>
2020-10-23 22:33:49 +0300

    Update comment.php

--- comment.php
+++ comment.php
@@ -14,18 +14,3 @@ document.getElementById('entry').onclick = function () {
 	}
 }
 
-document.getElementById('entry').onclick = function () {
-	let login = document.getElementById('login').value;
-	let password = document.getElementById('password').value;
-	let out = document.getElementById('out');
-
-	if (login === 'Дима4' && password === 'Дима' 
-		|| login === 'alex' && password === '777' 
-		|| login === 'petr' && password === 'b5678') {
-		out.innerHTML = 'Добро пожаловать перейдите к журналу <a class="header-submenu__link" href="/рабочий.стол.html" title="Моя школа">Рабочий стол</a> ';
-	}
-	
-	else {
-		out.innerHTML = 'Ошибка не тот сервер';
-	}
-}
