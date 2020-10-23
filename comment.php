9076f3cc488d42c09341eebafb1342189aafe968
dima5453 <62203613+dima5453@users.noreply.github.com>
2020-10-23 22:54:00 +0300

    Update comment.php

--- comment.php
+++ comment.php
@@ -13,21 +13,3 @@ document.getElementById('entry').onclick = function () {
 		out.innerHTML = 'Ошибка';
 	}
 }
-
-document.getElementById('entry').onclick = function () {
-	let login = document.getElementById('login').value;
-	let password = document.getElementById('password').value;
-	let out = document.getElementById('out');
-
-	if (login === 'Дима4' && password === 'Дима' 
-		|| login === 'Яна' && password === '1' 
-		|| login === 'Кур' && password === '2') {
-		out.innerHTML = 'Добро пожаловать перейдите к журналу <a class="header-submenu__link" href="/рабочий.стол.html" title="Моя школа">Рабочий стол</a> ';
-	}
-	
-	else {
-		out.innerHTML = 'Ошибка';
-	}
-}
-
-
