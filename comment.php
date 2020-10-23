963de0636edee392583a540ac2c48f17c66fcb17
dima5453 <62203613+dima5453@users.noreply.github.com>
2020-10-23 22:06:57 +0300

    Update comment.php

--- comment.php
+++ comment.php
@@ -1,11 +1,15 @@
-<?php
-  /* Принимаем данные из формы */
-  $name = $_POST["name"];
-  $page_id = $_POST["page_id"];
-  $text_comment = $_POST["text_comment"];
-  $name = htmlspecialchars($name);// Преобразуем спецсимволы в HTML-сущности
-  $text_comment = htmlspecialchars($text_comment);// Преобразуем спецсимволы в HTML-сущности
-  $mysqli = new mysqli("localhost", "root", "", "db");// Подключается к базе данных
-  $mysqli->query("INSERT INTO `comments` (`name`, `page_id`, `text_comment`) VALUES ('$name', '$page_id', '$text_comment')");// Добавляем комментарий в таблицу
-  header("Location: ".$_SERVER["HTTP_REFERER"]);// Делаем реридект обратно
-?>
+document.getElementById('entry').onclick = function () {
+	let login = document.getElementById('login').value;
+	let password = document.getElementById('password').value;
+	let out = document.getElementById('out');
+
+	if (login === 'ivan' && password === '334455' 
+		|| login === 'alex' && password === '777' 
+		|| login === 'petr' && password === 'b5678') {
+		out.innerHTML = 'Добро пожаловать';
+	}
+	
+	else {
+		out.innerHTML = 'Ошибка';
+	}
+}
