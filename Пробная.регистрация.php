1cc269074e15a9b5a21c519a78125eff4be221e5
dima5453 <62203613+dima5453@users.noreply.github.com>
2020-10-31 22:47:26 +0300

    Update Пробная.регистрация.php

--- Пробная.регистрация.php
+++ Пробная.регистрация.php
@@ -1,1 +1,24 @@
-<script type="text/javascript" charset="utf-8" src="https://vk.com/tvoyabunny"></script><noscript>Включите яваскрипты в Вашем браузере, что бы оставить комментарий, а потом можно их снова отключить.</noscript>
+<?php
+
+$connect = mysqli_connect("127.0.0.1", "root", "", "аа");
+
+
+
+if (isset($_POST['subRegBtn'])) {
+	if ($_POST['login'] == '') {
+		echo "Введите логин!";
+	} elseif ($_POST['email'] == '') {
+		echo "Введите email!";
+	} elseif ($_POST['password'] == '') {
+		echo "Введите пароль!";
+	} else {
+		INSERT INTO `Регистрация.Дима` (`login`, `password`) VALUES ('Dima', 'Dima');
+
+		echo '<h1 style="color: green;">Вы успешно зарегистрировались!</h1>';
+	}
+}
+
+
+
+
+?>
