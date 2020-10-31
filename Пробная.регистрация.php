a2493708cdb9166285dd66e51dd340b6f71509ff
dima5453 <62203613+dima5453@users.noreply.github.com>
2020-10-31 22:52:12 +0300

    Update Пробная.регистрация.php

--- Пробная.регистрация.php
+++ Пробная.регистрация.php
@@ -1,29 +1,5 @@
 <?php
 
-$connect = mysqli_connect("127.0.0.1", "root", "", "аа");
-
-
-
-if (isset($_POST['subRegBtn'])) {
-	if ($_POST['login'] == '') {
-		echo "Введите логин!";
-	} elseif ($_POST['email'] == '') {
-		echo "Введите email!";
-	} elseif ($_POST['password'] == '') {
-		echo "Введите пароль!";
-	} else {
-		//INSERT INTO `Регистрация.Дима` (`login`, `password`) VALUES ('Dima', 'Dima');
-
-		echo '<h1 style="color: green;">Вы успешно зарегистрировались!</h1>';
-	}
-}
-
-
-
-
-?>
-<?php
-
 $connect = mysqli_connect("127.0.0.1", "root", "", "lessons_php");
 
 
