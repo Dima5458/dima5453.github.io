d99b82b2e67ee278841725164740b1fd4c55629f
dima5453 <62203613+dima5453@users.noreply.github.com>
2020-10-31 23:41:34 +0300

    Update check.php

--- check.php
+++ check.php
@@ -1,1 +1,45 @@
-SELECT * FROM `Регистрация` WHERE 1
+-- phpMyAdmin SQL Dump
+-- version 5.1.0-dev
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 192.168.30.22
+-- Время создания: Окт 31 2020 г., 21:40
+-- Версия сервера: 10.4.8-MariaDB-1:10.4.8+maria~stretch-log
+-- Версия PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `Регистр`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Регистрация`
+--
+
+CREATE TABLE `Регистрация` (
+  `login` int(11) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `Регистрация`
+--
+
+INSERT INTO `Регистрация` (`login`, `password`) VALUES
+(54, 'Дима');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
