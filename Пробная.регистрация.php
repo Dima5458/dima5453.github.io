bff31bce95784a078b28b658a37b79f6eb63db7b
dima5453 <62203613+dima5453@users.noreply.github.com>
2020-10-31 23:18:33 +0300

    Update Пробная.регистрация.php

--- Пробная.регистрация.php
+++ Пробная.регистрация.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<?php echo "<title>Уроки PHP</title>"; ?>
-</head>
-<body>
-	
-
-
-<form method="post" action="/">
-	<input type="text" name="login" placeholder="Введите логин">
-	<br>
-	<input type="email" name="email" placeholder="Введите email">
-	<br>
-	<input type="password" name="password" placeholder="Введите пароль">
-	<br>
-	<button name="subRegBtn">Зарегистрироватся</button>
-</form>
-
-
-
 <?php
 
 $connect = mysqli_connect("127.0.0.1", "root", "", "lessons_php");
@@ -27,15 +5,13 @@ $connect = mysqli_connect("127.0.0.1", "root", "", "lessons_php");
 
 
 if (isset($_POST['subRegBtn'])) {
-	if ($_POST['login'] == '') {
-		echo "Введите логин!";
-	} elseif ($_POST['email'] == '') {
+	if ($_POST['login'] == '') 		
 		echo "Введите email!";
 	} elseif ($_POST['password'] == '') {
 		echo "Введите пароль!";
 	} else {
+				//INSERT INTO `Регистрация.Дима` (`login`, `password`) VALUES ('Dima', 'Dima');
 		$login = $_POST['login'];
-		$email = $_POST['email'];
 		$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 		mysqli_query($connect, "INSERT INTO `users` (`id`, `login`, `email`, `password`) VALUES (NULL, '$login', '$email', '$password');");
 		echo '<h1 style="color: green;">Вы успешно зарегистрировались!</h1>';
@@ -46,10 +22,3 @@ if (isset($_POST['subRegBtn'])) {
 
 
 ?>
-
-
-
-
-
-</body>
-</html>
