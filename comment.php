abd88e3c162d73425f460e8aec09286ef7245c9b
dima5453 <62203613+dima5453@users.noreply.github.com>
2020-11-21 22:02:40 +0300

    Update comment.php

--- comment.php
+++ comment.php
@@ -1,16 +1,2 @@
-document.getElementById('entry').onclick = function () {
-	let login = document.getElementById('login').value;
-	let password = document.getElementById('password').value;
-	let out = document.getElementById('out');
-
-	if (login === 'Дима' && password === 'Дима' 
-		|| login === 'Дима' && password === '777' 
-		|| login === 'Чер' && password === 'b5678') {
-		out.innerHTML = 'Добрый день вы зашли на профиль "Черненков Дмитрий Игоревич " организация "ТАКОЙ ШКОЛЫ НЕТ №4" нажмите на эту кнопку ---> Что-бы попасть в журнал <a class="header-submenu__link" href="/рабочий.стол.html" title="Моя школа">Рабочий стол</a> ';
-	}
-	
-	else {
-		out.innerHTML = 'Пользователя нет в этой системе "спросите у администратора"';
-	}
-}
-
+    R::setup( 'mysql:host=localhost;dbname=аа',
+        'root', '123' );
