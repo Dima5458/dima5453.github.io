73d5e7f1af84931e3885366d2e4afca0cc872c79
dima5453 <62203613+dima5453@users.noreply.github.com>
2021-02-21 22:51:16 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,10 +1,3 @@
 <p><a href="index.php">Главная</a> | <a href="contact.php">Контакты</a> | <a href="admin.php">Админка</a></p>
 <hr />
-Это страница авторизации.
-<br />
-<form method="post">
- Username: <input type="text" name="user" /><br />
- Password: <input type="password" name="pass" /><br />
- <input type="submit" name="submit" value="Войти" />
-</form>
-    <script src="https://static.school.mosreg.ru/client/react.vendor.bundle.js?v=8b433131" type="comment.php"></script>
+Это главная страница
