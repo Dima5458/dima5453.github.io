41f0a67aa76fb73a68347ccfe584a036062b9007
dima5453 <62203613+dima5453@users.noreply.github.com>
2021-02-21 22:47:36 +0300

    Update comment.php

--- comment.php
+++ comment.php
@@ -13,3 +13,10 @@ $pass = 'a029d0df84eb5549c641e04a9ef389e5';
  Password: <input type="password" name="pass" /><br />
  <input type="submit" name="submit" value="Войти" />
 </form>
+if($_POST['submit']){
+ if($admin == $_POST['user'] AND $pass == md5($_POST['pass'])){
+  $_SESSION['admin'] = $admin;
+  header("Location: admin.php");
+  exit;
+ }else echo '<p>Логин или пароль неверны!</p>';
+}
