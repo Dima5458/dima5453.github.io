ee76d92561e68c1100c6a49326199a9499dd551f
dima5453 <62203613+dima5453@users.noreply.github.com>
2021-02-21 22:44:33 +0300

    Update comment.php

--- comment.php
+++ comment.php
@@ -1,7 +1,15 @@
 <?php
-if($GET["password"] == "qwerty123"){
- print("Добро пожаловать!");
-}else{
- print("Доступ запрещен!");
-}
+session_start();
+
+$admin = 'admin';
+$pass = 'a029d0df84eb5549c641e04a9ef389e5';
 ?>
+<p><a href="index.php">Главная</a> | <a href="contact.php">Контакты</a> | <a href="admin.php">Админка</a></p>
+<hr />
+Это страница авторизации.
+<br />
+<form method="post">
+ Username: <input type="text" name="user" /><br />
+ Password: <input type="password" name="pass" /><br />
+ <input type="submit" name="submit" value="Войти" />
+</form>
