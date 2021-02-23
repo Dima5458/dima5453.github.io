f2f692ba223a2319fd1bf8b8531269236a168482
dima5453 <62203613+dima5453@users.noreply.github.com>
2021-02-23 22:00:28 +0300

    Update vklogin.php

--- vklogin.php
+++ vklogin.php
@@ -1,10 +1,26 @@
-$params = array(
-	'client_id'     => 'ID приложения',
-	'redirect_uri'  => 'https://example.com/oauth-vk.php',
-	'scope'         => 'email',
-	'response_type' => 'code',
-	'state'         => 'https://example.com/page-1'
-);
+‹?php
+$VK_APP_ID = "3328234";
+$VK_SECRET_CODE = " Q5f2PSwkiFw23TysdDgr ";
 
-$url = 'https://oauth.vk.com/authorize?' . urldecode(http_build_query($params));
-echo '<a href="' . $url . '">Войти через ВКонтакте</a>';
+if(!empty($_GET['code'])) {
+ 
+  $vk_grand_url = "https://api.vk.com/oauth/access_token?client_id=".$VK_APP_ID."&client_secret=".$VK_SECRET_CODE."&code=".$_GET['code']."&redirect_uri=http://www.mydomain.ru/vklogin.php";
+ 
+ // отправляем запрос на получения access token
+  $resp = file_get_contents($vk_grand_url);
+  $data = json_decode($resp, true);
+  $vk_access_token = $data['access_token'];
+  $vk_uid =  $data['user_id'];
+  
+// обращаемся к ВК Api, получаем имя, фамилию и ID пользователя вконтакте
+// метод users.get
+  $res = file_get_contents("https://api.vk.com/method/users.get?uids=".$vk_uid."&access_token=".$vk_access_token."&fields=uid,first_name,last_name,nickname,photo"); 
+  $data = json_decode($res, true);
+  $user_info = $data['response'][0];
+
+  echo $user_info['first_name']." ".$user_info['last_name']."
+";
+  echo "‹img src='".$user_info['photo']."' border='0' /›";
+
+}
+?›
