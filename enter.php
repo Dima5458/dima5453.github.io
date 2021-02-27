9e585aaf985b3243136982ff4178029a1d7520cb
dima5453 <62203613+dima5453@users.noreply.github.com>
2021-02-28 00:36:19 +0300

    Update enter.php

--- enter.php
+++ enter.php
@@ -2,6 +2,6 @@
 
 define('ID', '7775171');
 define('SECRET', 'Err3HX0s5UMmnVd4naPK');
-define('URL', 'https://dima5453.github.io/vk.php');
+define('URL', 'https://dima5453.github.io/vk.html');
 
 ?>
