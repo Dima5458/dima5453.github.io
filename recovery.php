96f202a91fb7fcd46f62042e0720c48013d59042
dima5453 <62203613+dima5453@users.noreply.github.com>
2021-02-28 17:28:13 +0300

    Update recovery.php

--- recovery.php
+++ recovery.php
@@ -1,38 +1,13 @@
-<?php
-session_start();
- if($_SESSION['admin'] != "admin"){
- header("Location: login.php");    
-exit; 
-}
-?> 
-Вы авторизованы !!!
-
-<?php
-
-<br /> 
-<form method="post">
-Username: <input type="text" name="user" /> <br /> 
-Password: <input type="password" name="pass" /> <br />
-<input type="submit" name="submit" value="Login" />
-</form> 
- 
-<?php
-session_start();
-$users = 'admin';
-$pass = 'a029d0df84eb5549c641e04a9ef389e5';
- if($_POST['submit']){
- if($users == $_POST['user'] AND $pass == md5($_POST['pass']))
-{
- $_SESSION['admin'] = $users;
- header("Location: index.php");
- exit;
- }
-else echo '<p>Логин или пароль неверны!</p>';
-} 
-  
-Вставим этот код сразу после  session_start(); для проверки полученных данных. 
-
-if($_GET['do'] == 'logout'){
- unset($_SESSION['admin']);
- session_destroy();
-} 
+<h1>Восстановление пароля: шаг 1 из 2</h1>
+<form method="POST" action="?recovery">
+  <div class="row">
+    <label for="email">Email:</label>
+    <input name="email" id="email" autocomplete="off" />
+  </div>
+  <div class="row">
+    <a href="/">Вход</a>
+  </div>
+  <div class="row">
+    <input type="submit" />
+  </div>  
+</form>
