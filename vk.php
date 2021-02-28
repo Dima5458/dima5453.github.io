f4b36587ef592f912d77a740cf513c9a6e9b0a24
dima5453 <62203613+dima5453@users.noreply.github.com>
2021-02-28 13:37:37 +0300

    Update vk.php

--- vk.php
+++ vk.php
@@ -3,3 +3,19 @@
 <input type="text" name="test">
 <input type="button" name="button">
 </form>
+
+<html>
+<head>
+<title>Главная страница</title>
+</head>
+<body>
+<?php
+$test=$_POST[text];
+echo  "Вы ввели:".$test;
+?>
+<form action="" method="POST">
+<input type="text" value="Введите текст здесь..." name="text">
+<input type="button" name="button">
+</form>
+</body>
+</html>
