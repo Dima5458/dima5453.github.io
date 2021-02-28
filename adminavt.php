d791fdfe8a47eabfa619f66443dc27b47294399d
dima5453 <62203613+dima5453@users.noreply.github.com>
2021-02-28 13:57:52 +0300

    Update adminavt.php

--- adminavt.php
+++ adminavt.php
@@ -1,21 +1,11 @@
-//Это vk.php
-<form action="index.php" method="POST">
-<input type="text" name="test">
-<input type="button" name="button">
-</form>
-
-<html>
-<head>
-<title>Главная страница</title>
-</head>
-<body>
-<?php
-$test=$_POST[text];
-echo  "Вы ввели:".$test;
-?>
-<form action="" method="POST">
-<input type="text" value="Введите текст здесь..." name="text">
-<input type="button" name="button">
-</form>
-</body>
-</html>
+$login = $_POST['login'];
+$pas = $_POST['password'];
+if ($login == 'Viktor' && $pas == 1122)
+  {
+  session_start();
+  $_SESSION['admin'] = true;
+  $script = 'adminpanel.php';
+  }
+else
+$script = 'avtadministrator.html';
+header("Location: $script");
